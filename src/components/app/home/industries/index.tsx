6c4511f2f254f231d8ab3dd7693fccd3b9d3fb74
uelthon <uelthon95@gmail.com>
2023-11-18 15:46:18 -0600

    lazy loading without ssr

--- src/components/app/home/industries/index.tsx
+++ src/components/app/home/industries/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Industry from './industry'
-import { industries } from '@/constants/home-tab-industries'
+// import Industry from './industry'
+// import { industries } from '@/constants/home-tab-industries'
 import { useTranslations } from 'next-intl'
 import Await from '@/components/shared/await'
 import dynamic from 'next/dynamic'
@@ -9,47 +9,48 @@ const IndustriesClient = dynamic(async () => await import('./industries-client')
   ssr: false
 })
 
-const Nodes = () => {
-  const t = useTranslations('home')
-  return (
-    <>
-      <Industry
-        data={industries[t('locale')].none.data}
-        title={industries[t('locale')].none.title}
-      />
-      <Industry
-        data={industries[t('locale')].consumers.data}
-        title={industries[t('locale')].consumers.title}
-      />
-      <Industry
-        data={industries[t('locale')].energy.data}
-        title={industries[t('locale')].energy.title}
-      />
-      <Industry
-        data={industries[t('locale')].manufacturing.data}
-        title={industries[t('locale')].manufacturing.title}
-      />
-      <Industry
-        data={industries[t('locale')].healthcare.data}
-        title={industries[t('locale')].healthcare.title}
-      />
-      <Industry
-        data={industries[t('locale')].retail.data}
-        title={industries[t('locale')].retail.title}
-      />
-      <Industry
-        data={industries[t('locale')].financial.data}
-        title={industries[t('locale')].financial.title}
-      />
-      <Industry
-        data={industries[t('locale')].media.data}
-        title={industries[t('locale')].media.title}
-      />
-    </>
-  )
-}
+// const Nodes = () => {
+//   const t = useTranslations('home')
+//   return (
+//     <>
+//       <Industry
+//         data={industries[t('locale')].none.data}
+//         title={industries[t('locale')].none.title}
+//       />
+//       <Industry
+//         data={industries[t('locale')].consumers.data}
+//         title={industries[t('locale')].consumers.title}
+//       />
+//       <Industry
+//         data={industries[t('locale')].energy.data}
+//         title={industries[t('locale')].energy.title}
+//       />
+//       <Industry
+//         data={industries[t('locale')].manufacturing.data}
+//         title={industries[t('locale')].manufacturing.title}
+//       />
+//       <Industry
+//         data={industries[t('locale')].healthcare.data}
+//         title={industries[t('locale')].healthcare.title}
+//       />
+//       <Industry
+//         data={industries[t('locale')].retail.data}
+//         title={industries[t('locale')].retail.title}
+//       />
+//       <Industry
+//         data={industries[t('locale')].financial.data}
+//         title={industries[t('locale')].financial.title}
+//       />
+//       <Industry
+//         data={industries[t('locale')].media.data}
+//         title={industries[t('locale')].media.title}
+//       />
+//     </>
+//   )
+// }
 
 export default function Industries () {
+  const t = useTranslations('locale')
   return (
     <section className='flex flex-col justify-start gap-8 items-center'>
       <p className='text-[4.375rem] leading-[87%] text-center [&>span]:text-it-blue-1 font-extrabold'>
@@ -58,7 +59,7 @@ export default function Industries () {
       <Await.Visible>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-8 w-full md:max-w-[1210px]'>
           <IndustriesClient
-            industriesNodes={<Nodes />}
+            locale={t('lang')}
           />
         </div>
       </Await.Visible>
--- src/components/app/home/industries/industries-client.tsx
+++ src/components/app/home/industries/industries-client.tsx
@@ -1,15 +1,48 @@
 'use client'
 import React from 'react'
+import Industry from './industry'
+import { industries } from '@/constants/home-tab-industries'
 
 interface Props {
-  industriesNodes: React.ReactNode
-
+  // industriesNodes: React.ReactNode
+  locale: string
 }
 
-export default function IndustriesClient ({ industriesNodes }: Props) {
+export default function IndustriesClient ({ locale }: Props) {
   return (
     <>
-      {industriesNodes}
+      <Industry
+        data={industries[locale].none.data}
+        title={industries[locale].none.title}
+      />
+      <Industry
+        data={industries[locale].consumers.data}
+        title={industries[locale].consumers.title}
+      />
+      <Industry
+        data={industries[locale].energy.data}
+        title={industries[locale].energy.title}
+      />
+      <Industry
+        data={industries[locale].manufacturing.data}
+        title={industries[locale].manufacturing.title}
+      />
+      <Industry
+        data={industries[locale].healthcare.data}
+        title={industries[locale].healthcare.title}
+      />
+      <Industry
+        data={industries[locale].retail.data}
+        title={industries[locale].retail.title}
+      />
+      <Industry
+        data={industries[locale].financial.data}
+        title={industries[locale].financial.title}
+      />
+      <Industry
+        data={industries[locale].media.data}
+        title={industries[locale].media.title}
+      />
     </>
   )
 }
