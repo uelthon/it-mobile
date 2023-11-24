7cc72d863ccd7d7855d957dd20e7a0f14fce676a
uelthon <uelthon95@gmail.com>
2023-11-23 22:23:54 -0600

    allow origin

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['www.lordsofit.com', 'www.lordsofit.com/es', 'www.lordsofit.com/en', 'localhost:3000', 'localhost:3015', 'localhost:3000/es', 'localhost:3015/es', 'localhost:3000/en', 'localhost:3015/en', 'phpstack-967373-4055673.cloudwaysapps.com']
+    }
+  }
 }
 
 const withNextIntl = require('next-intl/plugin')(
