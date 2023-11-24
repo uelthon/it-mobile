7afbf2df8308dd41bc3de8879467f528ea12f023
uelthon <uelthon95@gmail.com>
2023-11-24 09:16:36 -0600

    headers formobile server actions

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   experimental: {
     serverActions: {
-      allowedOrigins: ['www.lordsofit.com', 'www.lordsofit.com/es', 'www.lordsofit.com/en', 'localhost:3000', 'localhost:3015', 'localhost:3000/es', 'localhost:3015/es', 'localhost:3000/en', 'localhost:3015/en', 'phpstack-967373-4055673.cloudwaysapps.com']
+      allowedOrigins: ['www.lordsofit.com', 'lordsofit.com', 'www.lordsofit.com/es', 'www.lordsofit.com/en', 'localhost:3000', 'localhost:3015', 'localhost:3000/es', 'localhost:3015/es', 'localhost:3000/en', 'localhost:3015/en', 'phpstack-967373-4055673.cloudwaysapps.com']
     }
   }
 }
